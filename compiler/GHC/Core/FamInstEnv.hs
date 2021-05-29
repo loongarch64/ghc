cf610f140abe866a51ebc57300346d20824140bc
Ben Gamari <ben@smart-cactus.org>
2021-05-29 11:42:43 -0400

    size

--- compiler/GHC/Core/FamInstEnv.hs
+++ compiler/GHC/Core/FamInstEnv.hs
@@ -370,7 +370,7 @@ instance Outputable FamInstEnv where
   ppr (FamIE _ fs) = text "FamIE" <+> vcat (map ppr $ elemsRM fs)
 
 famInstEnvSize :: FamInstEnv -> Int
-famInstEnvSize (FamIE sz rm) = sz
+famInstEnvSize (FamIE sz _) = sz
 
 -- | Create a 'FamInstEnv' from 'Name' indices.
 -- INVARIANTS:
